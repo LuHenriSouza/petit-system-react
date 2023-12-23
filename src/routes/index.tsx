5aaca8c4e494fe1772bdd2080db6aa3f91781bd3
Luiz <luizhapsouza@gmail.com>
2023-12-22 21:46:34 -0300

    installing ui stuff and adding routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,13 @@
-export { };
+import { Button } from '@mui/material';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+export const AppRoutes = () => {
+
+    return (
+        <Routes>
+            <Route path="/home" element={<Button>test</Button>} />
+
+            <Route path="*" element={<Navigate to="/home" />} />
+        </Routes>
+    );
+}
