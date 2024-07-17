cdfba7ab7bea9443360901f8f5941f8e8696a6dc
Luiz <luizhapsouza@gmail.com>
2024-07-16 23:33:56 -0300

    feat: dinamic BackButton (need improvements)

--- src/pages/outflow/OutflowDetail.tsx
+++ src/pages/outflow/OutflowDetail.tsx
@@ -1,9 +1,9 @@
 import Swal from 'sweetalert2'
 import { format } from "date-fns";
 import './../../shared/css/sweetAlert.css'
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { LayoutMain } from "../../shared/layouts";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useSearchParams } from "react-router-dom";
 import ReplyAllRoundedIcon from '@mui/icons-material/ReplyAllRounded';
 import { Box, Button, Paper, Skeleton, TextField, Typography } from "@mui/material";
 import { FincashService, ICashOutflow, IFincash, ISupplier, OutflowService, SupplierService } from "../../shared/services/api";
@@ -15,11 +15,15 @@ export const OutflowDetail: React.FC = () => {
 	const [fincash, setFincash] = useState<IFincash>();
 	const [supplier, setSupplier] = useState<ISupplier>();
 	const [outflow, setOutflow] = useState<ICashOutflow>();
+	const [searchParams] = useSearchParams();
+
+	const caixa = useMemo(() => {
+		return searchParams.get('caixa');
+	}, [searchParams]);
 
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
-
 				const outflowFetch = await OutflowService.getById(Number(id));
 				if (outflowFetch instanceof Error) return 'outflow not found';
 				setOutflow(outflowFetch);
@@ -76,7 +80,7 @@ export const OutflowDetail: React.FC = () => {
 		<LayoutMain title={"Saída " + id} subTitle={"Saída " + id}>
 			<Paper sx={{ backgroundColor: '#fff', mr: 4, px: 3, py: 1 }}>
 				<Box display={'flex'} justifyContent={'space-between'}>
-					<Link to={'/saidas'}>
+					<Link to={caixa ? `/caixa/${caixa}` : '/saidas'}>
 						<Button variant="contained"> <ReplyAllRoundedIcon sx={{ mr: 1 }} /> Voltar </Button>
 					</Link>
 				</Box>
@@ -84,10 +88,11 @@ export const OutflowDetail: React.FC = () => {
 			<Paper sx={{ backgroundColor: '#fff', mr: 4, px: 3, py: 1, mt: 1 }}>
 				<Box margin={5}>
 					<Typography variant="h4" margin={1}>{fincash?.opener ? 'Caixa: ' + fincash.opener : <Skeleton sx={{ maxWidth: 300 }} />}</Typography>
+					<Typography variant="h5" margin={1}>{outflow?.created_at ? 'Dia: ' + format(outflow.created_at, 'dd / MM / yyyy') : <Skeleton sx={{ maxWidth: 300 }} />}</Typography>
 					<Typography variant="h5" margin={1}>{outflow?.created_at ? ("Hora: " + format(outflow.created_at, 'HH:mm')) : <Skeleton sx={{ maxWidth: 200 }} />}</Typography>
 					<Typography variant="h5" margin={1}>{outflow?.type ? 'Tipo: ' + outflow.type : <Skeleton sx={{ maxWidth: 300 }} />}</Typography>
 					{supplier && (<Typography variant="h6" margin={1}>{supplier.name ? 'Fornecedor: ' + supplier.name : <Skeleton sx={{ maxWidth: 200 }} />}</Typography>)}
-					<Typography variant="h6" margin={1}>{outflow?.value ? 'Valor: R$ ' + outflow.value : <Skeleton sx={{ maxWidth: 150 }} />}</Typography>
+					<Typography variant="h5" fontWeight={'bold'} margin={1}>{outflow?.value ? 'Valor: R$ ' + outflow.value : <Skeleton sx={{ maxWidth: 150 }} />}</Typography>
 				</Box>
 				<Box display={'flex'} flexDirection={'column'} gap={2} margin={2}>
 					<TextField
