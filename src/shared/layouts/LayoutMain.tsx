fa1355c17a5acb2a00a34a17006ea8ba3430360f
luizhapsouza <luizhapsouza@gmail.com>
2024-07-19 17:34:04 -0300

    Improve LayoutMain

--- src/shared/layouts/LayoutMain.tsx
+++ src/shared/layouts/LayoutMain.tsx
@@ -1,14 +1,17 @@
 import { Typography, useMediaQuery, useTheme } from '@mui/material';
 import { Box } from '@mui/system';
 import { TopBar } from '../components';
+import { Variant, } from '@mui/material/styles/createTypography';
 
 interface ILayoutMainProps {
     children: React.ReactNode;
-    title: string
-    subTitle: string
+    title: string;
+    subTitle: string;
+    subTitleVariant?: Variant;
+    subTitleSize?: number;
 }
 
-export const LayoutMain: React.FC<ILayoutMainProps> = ({ children, title, subTitle }) => {
+export const LayoutMain: React.FC<ILayoutMainProps> = ({ children, title, subTitle, subTitleSize, subTitleVariant = 'h6'}) => {
     const theme = useTheme();
     const smDown = useMediaQuery(theme.breakpoints.down('sm'))
 
@@ -24,7 +27,7 @@ export const LayoutMain: React.FC<ILayoutMainProps> = ({ children, title, subTit
                     <Typography variant={smDown ? 'h4' : 'h3'} whiteSpace={'nowrap'} textOverflow={'ellipsis'}>
                         {title}
                     </Typography>
-                    <Typography variant={'h6'} whiteSpace={'nowrap'} textOverflow={'ellipsis'} sx={{ ml: 2, fontWeight: 400, color: 'rgba(0,0,0,0.6)' }}>
+                    <Typography variant={subTitleVariant} whiteSpace={'nowrap'} textOverflow={'ellipsis'} sx={{ ml: 2, fontWeight: 400, color: 'rgba(0,0,0,0.6)' }} fontSize={subTitleSize}>
                         {subTitle}
                     </Typography>
                 </Box>
