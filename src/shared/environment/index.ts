80149905399f21bdd82da03cf66e5d2bd5044a02
luizhapsouza <luizhapsouza@gmail.com>
2024-07-29 15:50:09 -0300

    att ip

--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -14,5 +14,5 @@ export const Environment = {
     /**
      * Url base de consultado dos dados dessa aplicação
      */
-    URL_BASE: 'http://127.0.0.1:3333',
+    URL_BASE: 'http://177.30.39.225:3333',
 };
