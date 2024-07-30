f370ad45b7ba0fb5398489be6ee59baf5a802af1
luizhapsouza <luizhapsouza@gmail.com>
2024-07-30 17:33:22 -0300

    TESTING api.petitsystem.top

--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -14,5 +14,5 @@ export const Environment = {
     /**
      * Url base de consultado dos dados dessa aplicação
      */
-    URL_BASE: 'http://server.petitsystem.top:3333',
+    URL_BASE: 'http://api.petitsystem.top',
 };
