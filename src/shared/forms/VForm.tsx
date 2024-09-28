3b507c015234e01b0139a32c10a76746f15dc421
Luiz <luizhapsouza@gmail.com>
2024-09-28 00:24:57 -0300

    Fix some issues from new libs

--- src/shared/forms/VForm.tsx
+++ src/shared/forms/VForm.tsx
@@ -1,1 +1,19 @@
-export { Form as VForm } from '@unform/web';
+import React from 'react';
+import { Form } from '@unform/web';
+import { SubmitHandler, FormHandles } from '@unform/core';
+
+interface IProps {
+    children: React.ReactNode;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    onSubmit: SubmitHandler<any>;
+    placeholder?: unknown;
+}
+
+// Usando forwardRef para garantir o tipo do `ref`
+export const VForm = React.forwardRef<FormHandles, IProps>(({ children, onSubmit, placeholder }, ref) => {
+    return (
+        <Form ref={ref} onSubmit={onSubmit} onPointerEnterCapture={() => { }} onPointerLeaveCapture={() => { }} placeholder={placeholder}>
+            {children}
+        </Form>
+    );
+});
