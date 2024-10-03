af3b5c4f34bf80122f97ff851811fb5c2cacfd10
luizhapsouza <luizhapsouza@gmail.com>
2024-10-03 15:18:17 -0300

    EditFincash Completed

--- src/pages/fincash/EditFincash.tsx
+++ src/pages/fincash/EditFincash.tsx
@@ -11,6 +11,12 @@ import {
 	TableBody,
 	Typography,
 	Pagination,
+	Divider,
+	Dialog,
+	DialogTitle,
+	DialogContent,
+	DialogContentText,
+	DialogActions,
 } from "@mui/material";
 import * as yup from 'yup';
 import Swal from 'sweetalert2';
@@ -21,6 +27,7 @@ import AddIcon from "@mui/icons-material/Add";
 import EditIcon from '@mui/icons-material/Edit';
 import { VForm } from "../../shared/forms/VForm";
 import { LayoutMain } from "../../shared/layouts";
+import DeleteIcon from '@mui/icons-material/Delete';
 import HistoryIcon from '@mui/icons-material/History';
 import { nToBRL } from "../../shared/services/formatters";
 import { VTextField } from "../../shared/forms/VTextField";
@@ -29,13 +36,14 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import { Link, useParams, useSearchParams } from "react-router-dom";
 import ReplyAllRoundedIcon from '@mui/icons-material/ReplyAllRounded';
 import VisibilityRoundedIcon from '@mui/icons-material/VisibilityRounded';
-import { FincashService, ICashOutflow, IFincash, OutflowService } from "../../shared/services/api";
+import { FincashService, ICashOutflow, IFincash, OutflowService, SupplierService } from "../../shared/services/api";
+import { IMenuItens, VSelect } from "../../shared/forms/VSelect";
+import { EOutflowType } from "../outflow/enum/EOutflowType";
 
 const OUTFLOW_ROW_LIMIT = 5;
 
 export const EditFincash: React.FC = () => {
 	const { id } = useParams();
-	const [desc, setDesc] = useState('');
 
 	const [loading, setLoading] = useState(true);
 	const [cardLoading, setCardLoading] = useState(false);
@@ -43,7 +51,7 @@ export const EditFincash: React.FC = () => {
 	const [fincash, setFincash] = useState<IFincash>();
 	const [outflows, setOutflows] = useState<{ outflows: ICashOutflow[], total: number }>();
 
-	const formRef = useRef<FormHandles>(null);
+	const EditFormRef = useRef<FormHandles>(null);
 
 	const [outflowTotalCount, setOutflowTotalCount] = useState(0);
 	const [loadingOutflows, setLoadingoutflows] = useState(false);
@@ -71,7 +79,6 @@ export const EditFincash: React.FC = () => {
 				const CompleteFetch = await FincashService.getById(Number(id));
 				if (CompleteFetch instanceof Error) return 'Fincash not found';
 				const fincash = CompleteFetch;
-				fincash.obs && setDesc(fincash.obs);
 				if (!fincash.isFinished) {
 					const result = await FincashService.getTotalByFincash(fincash.id);
 					if (!(result instanceof Error)) {
@@ -122,79 +129,66 @@ export const EditFincash: React.FC = () => {
 		card: yup.number().required().min(0)
 	});
 
-	const cardSubmit = async (data: IFormData) => {
-		setCardLoading(true);
-		try {
-			data.card = data.card.replace(/[^\d,.-]/g, '').replace('.', '').replace(',', '.');
-			if (fincash) {
-				const swal = await Swal.fire({
-					title: 'Tem Certeza?',
-					text: `Registrar Cartão no Valor de "${nToBRL(Number(data.card))}" ?`,
-					icon: 'question',
-					iconColor: '#512DA8',
-					showCancelButton: true,
-					confirmButtonColor: '#512DA8',
-					cancelButtonColor: '#aaa',
-					cancelButtonText: 'Voltar',
-					confirmButtonText: 'Confirmar'
-				});
-				if (swal.isConfirmed) {
-					const dataValidated = await formValidation.validate(data, { abortEarly: false });
 
-					const result = await FincashService.registerCardValue(dataValidated.card, fincash.id);
-					if (result instanceof Error) {
-						return Swal.fire({
-							icon: "error",
-							title: "Atenção",
-							text: "Algum erro ocorreu ao tentar inserir o valor",
-							showConfirmButton: true,
-						});
-					}
+	// MODAL ENVIRONMENT
+	const selectManuItens: IMenuItens[] = [
+		{ text: 'Alimentação', value: EOutflowType.Alimentacao },
+		{ text: 'Transporte', value: EOutflowType.Transporte },
+		{ text: 'Fornecedor', value: EOutflowType.Fornecedor },
+		{ text: 'Sangria', value: EOutflowType.Sangria },
+		{ text: 'Outro', value: EOutflowType.Outro }
+	];
 
-					Swal.fire({
-						icon: "success",
-						title: "Sucesso!",
-						text: "Cartão registrado com sucesso!",
-						showConfirmButton: true,
-					});
-					setReload(1);
-				}
+	const OutflowFormRef = useRef<FormHandles>(null);
+	const [outflowLoading, setOutflowLoading] = useState(false);
+	const [isSupplier, setIsSupplier] = useState(false);
+	const [open, setOpen] = useState(false);
+	const [editOutflow, setEditOutflow] = useState<ICashOutflow>();
+	const [suppliers, setSuppliers] = useState<IMenuItens[]>([]);
 
-			}
-		} catch (e) {
-			console.log(e);
-		} finally {
-			setCardLoading(false);
-		}
+	const handleOpen = (outflow?: ICashOutflow) => {
+		setEditOutflow(outflow);
+		if (outflow?.type == EOutflowType.Fornecedor)
+			setIsSupplier(true);
+		setOpen(true);
+
 	}
 
-	const changeObs = async () => {
-		setLoading(true);
-		const result = await FincashService.updateObsById(Number(id), desc.trim());
+	const handleClose = () => {
+		setOpen(false);
+		setTimeout(() => {
+			setEditOutflow(undefined);
+			setIsSupplier(false);
+		}, 500)
+	}
 
-		if (result instanceof Error) {
-			return Swal.fire({
-				icon: "error",
-				title: "Atenção",
-				text: "Erro ao cadastrar Observação",
-				showConfirmButton: true,
-			});
+	const handleValueChange = async (selectedValue: string) => {
+		if (selectedValue === EOutflowType.Fornecedor) {
+			setIsSupplier(true);
+			try {
+				if (suppliers.length <= 0) {
+					const result = await SupplierService.getAll(undefined, undefined, 99999)
+					if (result instanceof Error) {
+						alert('Erro ao buscar Fornecedores');
+					} else {
+						result.data.map((data) => {
+							const object = { text: data.name, value: `${data.id}` }
+							setSuppliers((old) => [...old, object]);
+						})
+					}
+				}
+			} catch (e) {
+				alert(e);
+			}
+		} else {
+			setIsSupplier(false);
 		}
-
-		Swal.fire({
-			icon: "success",
-			title: "Sucesso!",
-			text: "Observação alterada com sucesso!",
-			showConfirmButton: true,
-		});
-		setLoading(false);
-		if (fincash) fincash.obs = desc;
 	}
 
 	return (
 		<LayoutMain
 			title={fincash ? `Caixa: ${format(fincash.created_at, 'dd/MM/yyyy')}` : ''}
-			subTitle={'Caixa: ' + fincash?.opener}
+			subTitle={'Editar Caixa'}
 		>
 			<Paper sx={{ backgroundColor: '#fff', mr: 4, px: 3, py: 1 }}>
 				<Box display={'flex'} justifyContent={'space-between'}>
@@ -204,25 +198,180 @@ export const EditFincash: React.FC = () => {
 				</Box>
 			</Paper>
 			<Paper sx={{ backgroundColor: '#fff', mr: 4, px: 3, py: 1, mt: 1 }}>
-				<Box display={'flex'} flexDirection={'column'} gap={2} margin={2}>
-					<TextField
-						rows={4}
-						fullWidth
-						multiline
-						sx={{ mt: 2 }}
-						value={desc}
-						onChange={(e) => setDesc(e.target.value)}
-						label="Observação"
-						id="elevation-multiline-static"
-						autoComplete="off"
-						disabled={loading}
+				<Box margin={5} display={'flex'} justifyContent={'space-between'}>
+					{fincash ?
+						<VForm onSubmit={() => { }} ref={EditFormRef}>
+							<Box minWidth={780}>
+								{/* <Box maxWidth={500}> */}
+								<VTextField name="name" label={'Nome'} valueDefault={fincash.opener} disabled={loading} />
+								<Divider sx={{ my: 4 }} />
+								<Box gap={2} display={'flex'} >
+									<VTextField name="value" label={'Início'} cash valueDefault={nToBRL(fincash.value)} disabled={loading} />
+									<VTextField name="finalValue" label={'Final'} cash valueDefault={nToBRL(fincash.finalValue)} disabled={loading} />
+								</Box>
+								<Divider sx={{ my: 2 }} />
+								<VTextField name="cardValue" cash label={'Cartão'} valueDefault={nToBRL(fincash.cardValue)} disabled={loading} />
+								<Divider sx={{ my: 4 }} />
+								{/* </Box> */}
+								<VTextField
+									name="obs"
+									rows={4}
+									fullWidth
+									multiline
+									valueDefault={fincash.obs ?? ''}
+									label="Observação"
+									id="elevation-multiline-static"
+									autoComplete="off"
+									disabled={loading}
+								/>
+								<Button variant="contained" size="large" sx={{ mt: 2 }} fullWidth disabled={loading} onClick={() => { }}>
+									EDITAR CAIXA
+								</Button>
+							</Box>
+						</VForm>
+						:
+						<Box display={'flex'} justifyContent={'center'} gap={5}>
+							<Box minWidth={300}>
+								<TextField label={'Nome'} disabled />
+								<Divider sx={{ my: 2 }} />
+								<Box gap={2} display={'flex'} flexDirection={'column'}>
+									<TextField label={'Início'} disabled />
+									<TextField label={'Final'} disabled />
+								</Box>
+							</Box>
+							<TextField label={'Cartão'} disabled />
+						</Box>
+					}
+					<Box border={1} minHeight={500} minWidth={600} my={2} sx={{ backgroundColor: '#eee' }} display={'flex'} alignItems={'center'} justifyContent={'space-between'} flexDirection={'column'} py={2}>
+						<Box display={'flex'} alignItems={'center'} flexDirection={'column'}>
+							<Button
+								variant="contained"
+								sx={{ my: 2 }}
+								onClick={() => handleOpen()}
+							>
+								<AddIcon sx={{ mr: 1 }} /> Adicionar Saída
+							</Button>
 
-					/>
-					<Button variant="contained" color="primary" size="large" sx={{ maxWidth: 250 }} disabled={loading || fincash?.obs == desc} onClick={changeObs}>
-						Alterar Observação
-					</Button>
+							<Box minWidth={400} minHeight={200}>
+								<Table>
+									<TableBody>
+										{outflows?.outflows.map((outflow) =>
+											<TableRow key={outflow.id}>
+												<TableCell>
+													<Typography variant="h5">
+														{outflow.type}
+													</Typography>
+												</TableCell>
+												<TableCell>
+													<Typography variant="h5">
+														{nToBRL(outflow.value)}
+													</Typography>
+												</TableCell>
+												<TableCell>
+													<Fab
+														size="small"
+														color="warning"
+														sx={{ mr: 2 }}
+														onClick={() => handleOpen(outflow)}
+													>
+														<EditIcon color="info" />
+													</Fab>
+													<Fab
+														size="small"
+														color="error"
+													>
+														<DeleteIcon color="info" />
+													</Fab>
+												</TableCell>
+											</TableRow>
+										)}
+									</TableBody>
+								</Table>
+							</Box>
+							{outflowTotalCount > 0 && (
+								<Pagination
+									sx={{ m: 2 }}
+									disabled={loadingOutflows}
+									page={Number(outflowPage)}
+									count={Math.ceil(outflowTotalCount / OUTFLOW_ROW_LIMIT)}
+									onChange={(_, newPage) => setSearchParams((old) => {
+										old.set("outflowPage", newPage.toString());
+										return old;
+									})}
+									siblingCount={0}
+								/>
+							)}
+						</Box>
+
+						<Box mb={5}>
+							<Typography variant="h5">
+								Total: {nToBRL(outflows?.total)}
+							</Typography>
+						</Box>
+					</Box>
 				</Box>
 			</Paper>
+			{/* ------------------- ------------------- EDIT/ADD MODAL ------------------- ------------------- */}
+			<Dialog
+				maxWidth='md'
+				open={open}
+				onClose={handleClose}
+				fullWidth
+				sx={{
+					"& .MuiDialog-paper":
+						{ backgroundColor: "#fff", }
+				}}>
+				<DialogTitle>{editOutflow ? 'Editar' : 'Adicionar'}</DialogTitle>
+				<DialogContent>
+					<DialogContentText mb={4}>
+						{editOutflow ? 'Editar' : 'Adicionar'} saída
+					</DialogContentText>
+					<Box minHeight={100} px={3}>
+						<VForm ref={OutflowFormRef} onSubmit={() => { }}>
+							<Box display={'flex'} flexDirection={'column'} gap={3} margin={3} minWidth={540}>
+								<Box display={'flex'} gap={5}>
+									<Box width={200}>
+										<VSelect
+											name="type"
+											label="Tipo"
+											menuItens={selectManuItens}
+											onValueChange={handleValueChange}
+											defaultSelected={editOutflow?.type}
+											messageError="Tipo não pode ser vazio"
+										/>
+									</Box>
+									{isSupplier &&
+										<Box width={300}>
+											<VSelect
+												name="supplier_id"
+												label="Fornecedor"
+												menuItens={suppliers.length > 0 ? suppliers : [{ text: "Nenhum fornecedor cadastrado", value: "" }]}
+												messageError="Fornecedor não pode ser vazio"
+												defaultSelected={editOutflow?.supplier_id ?? undefined}
+											/>
+										</Box>
+									}
+								</Box>
+								<VTextField name="value" label="Valor" cash sx={{ maxWidth: 170 }} autoComplete="off" valueDefault={nToBRL(editOutflow?.value)} />
+								<VTextField
+									name="desc"
+									rows={4}
+									fullWidth
+									multiline
+									label="Descrição"
+									id="elevation-multiline-static"
+									autoComplete="off"
+									valueDefault={editOutflow?.desc ?? undefined}
+								/>
+								<Button variant="contained" onClick={() => OutflowFormRef.current?.submitForm()} disabled={outflowLoading}>Confirmar</Button>
+							</Box>
+						</VForm>
+					</Box>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose} color={'error'} variant='outlined'>Cancelar</Button>
+				</DialogActions>
+			</Dialog>
 		</LayoutMain >
 	);
 };
